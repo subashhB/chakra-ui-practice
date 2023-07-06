cdc0b0399ec2ba07b2bae66b8af56a92ce71299d
Admin <subashbaidya9@gmail.com>
2023-07-06 18:02:59 +0545

    chore: Grid Layouts

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/Navbar"
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 export default function RootLayout() {
   return (
-    <div>
-      <Navbar/>
-      <Outlet />
-    </div>
-  )
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      <GridItem colSpan="1" p="30px" as="aside" bg="purple.400" minHeight="100vh">
+        <span>Side Bar</span>
+      </GridItem>
+      <GridItem as="main" p="40px" colSpan={5}>
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
+  );
 }
