56d6bbd48b46aaf380feac7d3444e0e814751b61
Admin <subashbaidya9@gmail.com>
2023-07-06 18:30:04 +0545

    chore: List and Icons

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Grid, GridItem } from "@chakra-ui/react";
+import Sidebar from "../components/Sidebar";
 
 export default function RootLayout() {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
       <GridItem colSpan={{ base: 6, lg: 2, xl: 1 }} p={{ base: "20px", lg: "30px" }} as="aside" bg="purple.400" minHeight={{ lg: "100vh"}}>
-        <span>Side Bar</span>
+        <Sidebar/>
       </GridItem>
       <GridItem as="main" p="40px" colSpan={{ base: 6, lg: 4, xl: 5 }}>
         <Navbar />
