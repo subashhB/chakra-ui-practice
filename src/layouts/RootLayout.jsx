4658918094983c09cac7d1106846401183cea379
Admin <subashbaidya9@gmail.com>
2023-07-07 08:29:09 +0545

    chore: Customizing the Themes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,8 +6,14 @@ import Sidebar from "../components/Sidebar";
 export default function RootLayout() {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-      <GridItem colSpan={{ base: 6, lg: 2, xl: 1 }} p={{ base: "20px", lg: "30px" }} as="aside" bg="purple.400" minHeight={{ lg: "100vh"}}>
-        <Sidebar/>
+      <GridItem
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        p={{ base: "20px", lg: "30px" }}
+        as="aside"
+        bg="brand.600"
+        minHeight={{ lg: "100vh" }}
+      >
+        <Sidebar />
       </GridItem>
       <GridItem as="main" p="40px" colSpan={{ base: 6, lg: 4, xl: 5 }}>
         <Navbar />
